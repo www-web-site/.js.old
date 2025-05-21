3afe97ff6fc072b579178ee1eb6ab4727ec4c645
www-web-site <329blogertop228@gmail.com>
2025-05-21 22:16:41 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,74 @@
+const one = document.getElementById('one')
+const two = document.getElementById('two')
+const three = document.getElementById('three')
+const four = document.getElementById('four')
+const five = document.getElementById('five')
+const six = document.getElementById('six')
+const seven = document.getElementById('seven')
+const eight = document.getElementById('eight')
+const nine = document.getElementById('nine')
+const ten = document.getElementById('ten')
+const eleven = document.getElementById('eleven')
+const twelve = document.getElementById('twelve')
+
+one.onclick = function() {
+    const oneT = prompt('Qiyqiriq сет nechta zakaz qilmoqchisiz?')
+    confirm(`${oneT} qiyqiriq cet ${oneT*45000} boladi \n Buyurmoqchimisiz?`)
+    alert('zakaz qabul qilindi')
+}
+
+two.onclick = function() {
+    const twoT = prompt('Снек сет nechta zakaz qilmoqchisiz?')
+    confirm(`${twoT} Снек сет ${twoT*79000} boladi \n Buyurmoqchimisiz?`)
+    alert('zakaz qabul qilindi')
+}
+three.onclick = function() {
+    const threeT = prompt('Классик сет nechta zakaz qilmoqchisiz?')
+    confirm(`${threeT} Классик сет ${threeT*34000} boladi \n Buyurmoqchimisiz?`)
+    alert('zakaz qabul qilindi')
+}
+four.onclick = function() {
+    const fourT = prompt('Лонгер рингс сет nechta zakaz qilmoqchisiz?')
+    confirm(`${fourT} Лонгер рингс сет ${fourT*45000} boladi \n Buyurmoqchimisiz?`)
+    alert('zakaz qabul qilindi')
+}
+five.onclick = function() {
+    const fiveT = prompt('Биг сет nechta zakaz qilmoqchisiz?')
+    confirm(`${fiveT} Биг сет ${fiveT*62000} boladi \n Buyurmoqchimisiz?`)
+    alert('zakaz qabul qilindi')
+}
+six.onclick = function() {
+    const sixT = prompt('Лестер сет nechta zakaz qilmoqchisiz?')
+    confirm(`${sixT} Лестер сет ${sixT*53000} boladi \n Buyurmoqchimisiz?`)
+    alert('zakaz qabul qilindi')
+}
+seven.onclick = function() {
+    const sevenT = prompt('Скул сет nechta zakaz qilmoqchisiz?')
+    confirm(`${sevenT} Скул сет ${sevenT*36000} boladi \n Buyurmoqchimisiz?`)
+    alert('zakaz qabul qilindi')
+}
+eight.onclick = function() {
+    const eightT = prompt('Комбо сет Большой nechta zakaz qilmoqchisiz?')
+    confirm(`${eightT} Комбо сет Большой ${eightT*20000} boladi \n Buyurmoqchimisiz?`)
+    alert('zakaz qabul qilindi')
+}
+nine.onclick = function() {
+    const nineT = prompt('Do’stlar 1х nechta zakaz qilmoqchisiz?')
+    confirm(`${nineT} Do’stlar 1х ${nineT*43000} boladi \n Buyurmoqchimisiz?`)
+    alert('zakaz qabul qilindi')
+}
+ten.onclick = function() {
+    const tenT = prompt('Do’stlar 2х nechta zakaz qilmoqchisiz?')
+    confirm(`${tenT} Do’stlar 2х ${tenT*79000} boladi \n Buyurmoqchimisiz?`)
+    alert('zakaz qabul qilindi')
+}
+eleven.onclick = function() {
+    const elevenT = prompt('Do’stlar 4х nechta zakaz qilmoqchisiz?')
+    confirm(`${elevenT} Do’stlar 4х ${elevenT*149000} boladi \n Buyurmoqchimisiz?`)
+    alert('zakaz qabul qilindi')
+}
+twelve.onclick = function() {
+    const twelveT = prompt('Острый Do’stlar 1хт nechta zakaz qilmoqchisiz?')
+    confirm(`${twelveT} Острый Do’stlar 1x ${twelveT*43000} boladi \n Buyurmoqchimisiz?`)
+    alert('zakaz qabul qilindi')
+}
